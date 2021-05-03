87228be5f3fd809c96ca8b561bd136357a601301
Rohit Kumar <rohit@eloelo.in>
2021-05-03 18:26:24 +0530

    base commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import "assets/vendor/nucleo/css/nucleo.css";
+import "assets/vendor/font-awesome/css/font-awesome.min.css";
+import "assets/scss/rohit-resume-react.scss?v1.1.0";
+
+import Index from "views/Index.js";
+import Landing from "views/examples/Landing.js";
+import Login from "views/examples/Login.js";
+import Profile from "views/examples/Profile.js";
+import Register from "views/examples/Register.js";
+
+ReactDOM.render(<Profile />, document.getElementById("root"));
